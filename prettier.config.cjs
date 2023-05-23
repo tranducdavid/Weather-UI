b2b2e0e41e61d5a608d6cbe54817dbf6c74e98a9
Ceirra <ducovatranovam@gmail.com>
2023-05-23 21:43:11 +0200

    Add Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 200,
+  endOfLine: 'crlf',
+}
